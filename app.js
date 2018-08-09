4f73d6e06eb2c8ea78bf9f6006f6a43e485d69fd
Henrique de O. Fadoni <HenriqueFadoni@users.noreply.github.com>
2018-08-09 16:59:09 -0300

    Creating an Initialization Function

--- app.js
+++ app.js
@@ -33,12 +33,21 @@ var UIController = (function(){
 //GLOVAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function(){
+        var DOM = UICtrl.getDOMstrings();
 
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function(event){
+            if (event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+
+        });
+    };
+    
     var ctrlAddItem = function(){
         // 1. Get the field input data
         var input = UICtrl.getinput();
-        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -47,16 +56,14 @@ var controller = (function(budgetCtrl, UICtrl){
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-    }
+    };
 
-    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event){
-        
-        if (event.keyCode === 13 || event.which === 13){
-            ctrlAddItem();
+    return{
+        init:function(){
+            console.log('Application has started.');
+            setupEventListeners();
         }
-
-    });
-
+    }
 })(budgetController, UIController);
+
+controller.init();
